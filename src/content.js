6d64acbc82172cad59a6268aa175ed109ceeb53a
Masaki Osugi <masaki.osugi@gmail.com>
2022-07-18 08:11:40 +0900

    Initial Commit

---
+++ src/content.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const samlInfo = document.querySelector('.news .mb-2') || document.querySelector('.container-md .mb-2');
+    if(samlInfo) {
+        samlInfo.style.display = "none";
+    }
+}
