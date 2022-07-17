2ace679fb2575a8a27d388cbda6966ce252a6fd2
Masaki Osugi <masaki.osugi@gmail.com>
2022-07-18 08:31:28 +0900

    Support Issue / Pull requests Page and Check SAML Auth content

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    const samlInfo = document.querySelector('.news .mb-2') || document.querySelector('.container-md .mb-2');
-    if(samlInfo) {
+    const samlInfo = document.querySelector('.news .mb-2') || document.querySelector('.container-md .mb-2') || document.querySelector('.container-lg .mb-2');
+    if(samlInfo.textContent.match(/.*Single\ssign-on.*/)) {
         samlInfo.style.display = "none";
     }
 }
